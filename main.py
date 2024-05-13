011e76f2ec6832ca98b5a68a7a6dce4a38526df0
agrhigham <ahigham93@gmail.com>
2024-05-13 19:47:17 +0100

    Formatted the word and character counts in to a report

--- main.py
+++ main.py
@@ -3,9 +3,13 @@ def main():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
     num_words = get_num_words(text)
+    print(f"--- Begin report of {path_to_file} ---")
     print(f"{num_words} words found in the document")
     chars_dict = get_character_count(text)
-    print (chars_dict)
+    list_of_dicts = get_list_of_dicts(chars_dict)
+    list_of_dicts.sort(reverse=True, key=sort_on)
+    for item in list_of_dicts:
+        print (f"The {item['character']} character was found {item['count']} times")
    
    
     
@@ -21,16 +25,23 @@ def get_character_count(text):
     character_count = {}
     lowered_text = text.lower()
     for char in lowered_text:
-        if char in character_count:
-            character_count[char] += 1
-        else:
-            character_count[char] = 1
+        if char.isalpha():
+            if char in character_count:
+                character_count[char] += 1
+            else:
+                character_count[char] = 1
     return character_count
+
+def get_list_of_dicts(chars_dict):
+    list_of_dicts = []
+    for char, count in chars_dict.items():
+        new_dict = {"character": char, "count": count}
+        list_of_dicts.append(new_dict)
+    return list_of_dicts
+
+def sort_on(list_of_dicts):
+    return list_of_dicts["count"]
+
+
     
-
-
-
-
-
-
 main()
