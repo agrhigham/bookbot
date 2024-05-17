381d325ff0fe26a6925d8d0aa2a8aaa24ef4a20d
agrhigham <ahigham93@gmail.com>
2024-05-17 16:59:13 +0100

    Updated code to find and replace a target word

--- Find and Replace.py
+++ Find and Replace.py
@@ -1,23 +1,28 @@
 def main():
     path_to_file = "books/code.txt"
     text = get_book_text(path_to_file)
-    words = get_words(text)
     target = "test"
-    find_word_in_text(words, target)
+    replacement = "exam"
+    new_text = replace_word_in_text(text, target, replacement)
+    output_file = "books/modified_code.txt"
+    save_new_text(output_file, new_text)
 
 def get_book_text(path_to_file):
     with open(path_to_file)as f:
         return f.read()
-    
-def get_words(text):
+
+def replace_word_in_text(text, target, replacement):
     words = text.split()
-    return words
-
-def find_word_in_text(words, target):
-    count = 0
+    new_words = []
     for word in words:
-        if word == target:
-            count += 1
-    print(count)
+        if word.lower() == target.lower():
+            new_words.append(replacement)
+        else:
+            new_words.append(word)
+    return ' '.join(new_words)
 
-main ()
+def save_new_text(path_to_file, text):
+    with open(path_to_file, 'w') as f:
+        f.write(text)
+
+main()
